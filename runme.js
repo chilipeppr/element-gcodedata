f0bc9a16f19d7d15fe13251b7e740b836bdcf248
John Lauer <jlauer@chilipeppr.com>
2016-02-18 00:03:02 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- runme.js
+++ runme.js
@@ -167,6 +167,11 @@ String.prototype.regexIndexOf = function(regex, startpos) {
 var widgetSrc, widget, id, deps, cpdefine, requirejs, cprequire_test;
 var widgetDocs = {};
 
+/**
+ * This method will actually eval your widget.js to bring it into memory
+ * so it can be iterated and parsed using standard javascript. This lets
+ * us generate docs. If your js doesn't eval, this method will crash.
+ */
 var isEvaled = false;
 var evalWidgetJs = function() {
   
@@ -661,6 +666,25 @@ var generateWidgetDocs = function() {
     <script type="text/javascript" charset="utf-8" src="//i2dcui.appspot.com/js/bootstrap/bootstrap_3_1_1.min.js"></script>
     
     <style type='text/css'>
+    div#editor-box {
+      border: 2px dashed #7f7f7f;
+      text-align: center;
+      vertical-align: middle;
+      padding: 10px 10px 10px 10px;
+      line-height: 10px;
+      max-height: 500px;
+      max-width: 100%;
+    }
+      
+    div#editor-box > img {
+      max-width: 500px;
+      max-height: 500px;
+    }
+      
+    .contain {
+      background-size: 100%;
+      background-repeat: no-repeat;
+    }
     </style>
     
     <script type='text/javascript'>
@@ -730,6 +754,115 @@ var generateWidgetDocs = function() {
         });
       }
       
+      // Created by STRd6
+      // MIT License
+      // jquery.paste_image_reader.js
+      (function ($) {
+          var defaults;
+          $.event.fix = (function (originalFix) {
+              return function (event) {
+                  event = originalFix.apply(this, arguments);
+                  if (event.type.indexOf('copy') === 0 || event.type.indexOf('paste') === 0) {
+                      event.clipboardData = event.originalEvent.clipboardData;
+                  }
+                  return event;
+              };
+          })($.event.fix);
+          defaults = {
+              callback: $.noop,
+              matchType: /image.*/
+          };
+          return $.fn.pasteImageReader = function (options) {
+              if (typeof options === "function") {
+                  options = {
+                      callback: options
+                  };
+              }
+              options = $.extend({}, defaults, options);
+              return this.each(function () {
+                  var $this, element;
+                  element = this;
+                  $this = $(this);
+                  return $this.bind('paste', function (event) {
+                      var clipboardData, found;
+                      found = false;
+                      clipboardData = event.clipboardData;
+                      return Array.prototype.forEach.call(clipboardData.types, function (type, i) {
+                          var file, reader;
+                          if (found) {
+                              return;
+                          }
+                          if (type.match(options.matchType) || clipboardData.items[i].type.match(options.matchType)) {
+                              file = clipboardData.items[i].getAsFile();
+                              reader = new FileReader();
+                              reader.onload = function (evt) {
+                                  return options.callback.call(element, {
+                                      dataURL: evt.target.result,
+                                      event: evt,
+                                      file: file,
+                                      name: file.name
+                                  });
+                              };
+                              reader.readAsDataURL(file);
+                              //snapshoot();
+                              return found = true;
+                          }
+                          backgroundImage
+                      });
+                  });
+              });
+          };
+      })(jQuery);
+      
+      
+      $("html").pasteImageReader(function (results) {
+              var dataURL, filename;
+              filename = results.filename, dataURL = results.dataURL;
+              $data.text(dataURL);
+              $size.val(results.file.size);
+              $type.val(results.file.type);
+              $test.attr('href', dataURL);
+              var img = document.createElement('img');
+              img.src = dataURL;
+              var w = img.width;
+              var h = img.height;
+              $width.val(w); $height.val(h);
+              $("div#editor-box").height(h);
+              return $(".active").css({
+                  backgroundImage: "url(" + dataURL + ")"
+              }).data({ 'width': w, 'height': h });
+          });
+      
+          var $data, $size, $type, $test, $width, $height;
+          $(function () {
+              $data = $('.data');
+              $size = $('.size');
+              $type = $('.type');
+              $test = $('#test');
+              $width = $('#width');
+              $height = $('#height');
+              $('.target').on('click', function () {
+                  var $this = $(this);
+                  var bi = $this.css('background-image');
+                  if (bi != 'none') {
+                      $data.text(bi.substr(4, bi.length - 6));
+                  }
+      
+                  $('.active').removeClass('active');
+                  $this.addClass('active');
+      
+                  $this.toggleClass('contain');
+      
+                  $width.val($this.data('width'));
+                  $height.val($this.data('height'));
+                  if ($this.hasClass('contain')) {
+                      $this.css({ 'width': $this.data('width'), 'height': $this.data('height'), 'z-index': '10' });
+                  } else {
+                      $this.css({ 'width': '', 'height': '', 'z-index': '' });
+                  }
+              });
+          });
+      
       function init() {
         $('.btn-pushtogithub').click(ajaxPushToGithub);
         $('.btn-pullfromgithub').click(ajaxPullFromGithub);
@@ -754,6 +887,11 @@ var generateWidgetDocs = function() {
       <button class="btn btn-xs btn-default btn-mergetemplate">Merge the ChiliPeppr Template to this Repo</button>
       <div class="hidden well ajax-results" style="margin-bottom:0;">
         Results
+      </div>
+      
+      Note: Paste Print screen or captured files here to attach
+      <br />
+      <div id="editor-box" class="target" contenteditable="true">
       </div>
       
       <h1 class="page-header" style="margin-top:20px;">$pubsub-id</h1>
